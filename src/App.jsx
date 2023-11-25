e21842aba04fc0957d0662fdcec1ff35f85fd177
Chinmaya-V <chinmayav04@gmail.com>
2023-11-25 11:58:46 +0530

    Created Search box and App styles added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import SearchBox from "./components/search-box/search-box.component";
+
+function App() {
+  return (
+    <div>
+      <SearchBox />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import './search-box.styles.css'
+
+const SearchBox = () => {
+  return <input type="search" className="search-box" />;
+};
+
+export default SearchBox;
